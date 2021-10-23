4f50976210562fb86791e4b86588b53f053e56d5
Harsh <vharshverma18@gmail.com>
2021-10-23 23:59:36 +0530

    movies

--- models/display.py
+++ models/display.py
@@ -17,9 +17,6 @@ def catalog1():
 def catalog2():
     return render_template('catalog2.html')  
 
-def F9():
-    return render_template('F9.html') 
-
 def details2():
     return render_template('details2.html')          
 
@@ -33,4 +30,70 @@ def about():
     return render_template('about.html')     
 
 def booking():
-    return render_template('booking.html')         
+    return render_template('booking.html')    
+
+def bell():
+    return render_template('bell.html')  
+
+def bhuj():
+    return render_template('bhuj.html')               
+
+def chehre():
+    return render_template('chehre.html')  
+
+def coolie():
+    return render_template('coolie.html')
+
+def f9():
+    return render_template('f9.html')  
+
+def conjuring():
+    return render_template('conjuring.html')    
+
+def haseen():
+    return render_template('haseen.html')  
+
+def hellocharlie():
+    return render_template('hellocharlie.html')     
+
+def hungama2():
+    return render_template('hungama2.html')  
+
+def indoo():
+    return render_template('indoo.html') 
+
+def jagame():
+    return render_template('jagame.html')  
+
+def ludo():
+    return render_template('ludo.html')     
+
+def mimi():
+    return render_template('mimi.html')  
+
+def mumbaisaga():
+    return render_template('mumbaisaga.html')         
+
+def radhe():
+    return render_template('radhe.html')  
+
+def roohi():
+    return render_template('roohi.html')   
+
+def saina():
+    return render_template('saina.html')  
+
+def sherni():
+    return render_template('sherni.html')      
+
+def shershah():
+    return render_template('shershah.html')  
+
+def shiddat():
+    return render_template('shiddat.html')        
+
+def soorarai():
+    return render_template('soorarai.html')  
+
+def thailavi():
+    return render_template('thailavi.html')           
